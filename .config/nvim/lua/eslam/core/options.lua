685eb49fcc510dae9d693c20556e1d35589dfe05
Eslam Osama <eslamebrahim1301@gmail.com>
2024-06-11 12:32:39 +0300

    some updates

--- .config/nvim/lua/eslam/core/options.lua
+++ .config/nvim/lua/eslam/core/options.lua
@@ -13,21 +13,30 @@ opt.autoindent = true
 -- line wrap
 opt.wrap = false
 
+-- allow mouse use
+opt.mouse = "a"
+
 -- search optoins
 opt.ignorecase = true
 opt.smartcase = true
 
-opt.cursorline = true -- cursorline
+-- cursorline
+opt.cursorline = true
 
 -- theme options
 opt.termguicolors = true
 opt.background = "dark"
 opt.signcolumn = "yes"
 
-opt.backspace = "indent,eol,start" -- allow backspace on indentation and end of line
+-- allow backspace on indentation and end of line
+opt.backspace = "indent,eol,start"
 
-opt.clipboard:append("unnamedplus") -- use system clipboard
+-- use system clipboard
+opt.clipboard:append("unnamedplus")
 
 -- window splitting
 opt.splitright = true
 opt.splitbelow = true
+
+-- Remove ~ at end of file
+opt.fillchars:append({ eob = " " })
